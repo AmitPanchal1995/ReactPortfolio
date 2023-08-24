1e20910a209d721efe657538e0a737f99782ee12
Void <shivanshu63073@gmail.com>
2023-08-24 23:05:16 +0530

    "update"

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 
-import { FaBars, FaTimes, FaGithub, FaLinkedin, FaFacebook } from 'react-icons/fa';
+import { FaBars, FaTimes, FaGithub, FaLinkedin} from 'react-icons/fa';
 import { HiOutlineMail } from 'react-icons/hi';
 import { BsFillPersonLinesFill } from 'react-icons/bs'
 import namelogo from "../assests/namelogo.png";
