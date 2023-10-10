2402dbe6848751809dcd075edf428a20d1606907
Void <shivanshu63073@gmail.com>
2023-10-10 18:37:20 +0530

    Another work sample added

--- src/components/Work.js
+++ src/components/Work.js
@@ -6,6 +6,7 @@ import TODO from '../assests/TODO.png';
 import GIT from '../assests/GIT.png'
 import EduPIC from '../assests/React-App.png';
 import Career from '../assests/career.png';
+import Tamana from '../assests/TamannaPic.png';
 
 
 export default function Work() {
@@ -26,6 +27,21 @@ export default function Work() {
                     {/* Grid */}
 
 
+
+
+                    <div
+                        style={{ backgroundImage: `url(${Tamana})` }}
+                        className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+                    >
+                        {/* Hover effect */}
+                        <div className="opacity-0 group-hover:opacity-100">
+                            <span className="text-2xl font-bold text-white tracking-wider">React JS Application</span>
+                            <div className="pt-8 text-center">
+                                <a href="https://tamanna-eta.vercel.app/"><button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Demo</button></a>
+                                <a href="https://github.com/Shivanshu002/multipage_website"><button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Code</button></a>
+                            </div>
+                        </div>
+                    </div>
 
 
                     <div
